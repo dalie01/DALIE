79a8d0582ced9d9b518e47971c7d1ca5ce4f28fb
DALiE de <dalie@dalieweb.de>
2018-01-13 07:09:05 +0100

    change print by s1 attribute

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
-  
+
+  private public static String S1 = "HelloWorld";
+
   public static void main(String[] args) {
       System.out.println("Hello");
+      System.out.println(S1);
   }
-  
+
 }
