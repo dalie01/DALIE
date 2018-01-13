c32a1a56f874881ed0c464eed7ed2476a9866c78
DALiE de <dalie@dalieweb.de>
2018-01-13 07:26:27 +0100

    bug fix

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
-  private public static String S1 = "HelloWorld";
+  private static String S1 = "HelloWorld";
 
   public static void main(String[] args) {
       System.out.println("Hello");
